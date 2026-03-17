e6bfb03afe7dc4a30abfac6cbd9a00e92153c278
Johan Andres Cardenas Hernandez <tecnogas2010@gmail.com>
2026-03-17 19:45:23 +0000

    Gen de excusa actualizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,71 @@
-import "bootstrap";
 import "./style.css";
 
+window.onload = function () {
+  const who = [
+    "A lonely merchant",
+    "My neighbor",
+    "A weird clown",
+    "The mailman",
+    "My dog",
+    "An old pirate",
+    "A confused police officer",
+    "A ninja turtle"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const action = [
+    "kicked",
+    "destroyed",
+    "stole",
+    "broke",
+    "painted",
+    "hid",
+    "pushed",
+    "crushed"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const what = [
+    "my car",
+    "my homework",
+    "my laptop",
+    "my backpack",
+    "my phone",
+    "my shoes",
+    "my project",
+    "my bicycle"
+  ];
+
+  const when = [
+    "near the toilet",
+    "before the class",
+    "during my lunch",
+    "while I was sleeping",
+    "in the parking lot",
+    "inside the kitchen",
+    "next to the elevator",
+    "when I was exercising"
+  ];
+
+  function getRandomItem(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+
+  function generateExcuse() {
+    const randomWho = getRandomItem(who);
+    const randomAction = getRandomItem(action);
+    const randomWhat = getRandomItem(what);
+    const randomWhen = getRandomItem(when);
+
+    return {
+      firstLine: `${randomWho} ${randomAction} ${randomWhat}`,
+      secondLine: `${randomWhen}`
+    };
+  }
+
+  const excuse = generateExcuse();
+
+  document.querySelector("#intro").innerHTML =
+    "OMG! you will not believe me but...";
+  document.querySelector("#excuse-line-1").innerHTML = excuse.firstLine;
+  document.querySelector("#excuse-line-2").innerHTML = excuse.secondLine;
 };
